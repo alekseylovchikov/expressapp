414128e6e6ba4a74e489850901a3d63ab4cf1010
Aleksey Lovchikov <svdmusic@gmail.com>
2015-10-04 20:18:10 +0300

    nodemailer change settings

--- routes/contact.js
+++ routes/contact.js
@@ -12,14 +12,14 @@ router.post('/send', function(req, res, next) {
 	var transporter = nodemailer.createTransport({
 		service: 'Gmail',
 		auth: {
-			user: 'svdmusic@gmail.com',
-			pass: '220988221090dD'
+			user: 'EMAIL@MAIL.COM',
+			pass: 'EMAIL PASS'
 		}
 	});
 
 	var mainOptions = {
-		from: 'Aleksey Lovchikov <aleksey.lovchikov@aol.com>',
-		to: 'svdmusic@gmail.com',
+		from: 'Aleksey Lovchikov <EMAIL@MAIL.COM>',
+		to: 'EMAIL@MAIL.COM',
 		subject: 'Web Site message',
 		text: 'Hello from node... name: ' + req.body.username + '\nemail: ' + req.body.email + '\nmessage: ' + req.body.message,
 		html: '<p>You got a new message...</p><p>Username: ' + req.body.username + '<br />Email: ' + req.body.email + '<br />Message: ' + req.body.message + '</p>'
